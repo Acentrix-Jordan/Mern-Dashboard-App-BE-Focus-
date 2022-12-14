5dd10f992632625aa3e358923b7faf7bd54abe08
Acentrix-Jordan <biggyj_dev@outlook.com>
2022-12-14 23:11:05 +0000

    All config complete and made a start on the App bar

---
+++ README.md
@@ -0,0 +1,39 @@
+#React MERN Dashboard App
+
+This project is from a code along on youtube by EdRoh.
+
+For this particular project we will be building a full stack application with a focus on the backend technology.
+
+With me being a Front-End Developer, I would really to expand my knowledge and get a basic understanding of what is happening on the backend so I can talk to the BEE's and be more specific. Or maybe even try and become a Full Stack Engineer. Who knows?
+
+I have not just blindly followed this tutorial and have rewatched many sections to truely understand is happening. 
+
+You will see there is a lot of comments on the build. This is for my own knowledge and so I can reference the code in future.
+
+## Backend Tech Stack
+Express -> Framework for our API's
+Body Parser -> Parsing our data coming in
+cors -> Cross Origin Resource Sharing
+dotenv -> Enviroment Variables
+helmet -> Protecting API's
+morgan -> Logging our API calls
+mongoose -> Handling Mongoose DB Calls
+Nodemon -> Live Server Reload
+
+## Frontend Tech Stack
+React
+React Redux
+Redux JS Toolkit
+React Datepicker
+React Router DOM @ 6
+@mui/material
+@mui/icons-material
+@mui/x-data-grid
+@emotion/react
+@emotion/styled
+@nivo/core
+@nivo/bar
+@nivo/geo
+@nivo/pie
+
+https://www.youtube.com/watch?v=0cPCMIuDk2I&t=422
---
+++ server/index.js
@@ -0,0 +1,70 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+
+/* Routes */
+import generalRoutes from "./routes/general.js";
+import clientRoutes from "./routes/client.js";
+import managementRoutes from "./routes/management.js";
+import salesRoutes from "./routes/sales.js";
+
+/* Configuration */
+dotenv.config();
+const app = express();
+
+/* 
+    In the tutorial the dev did app.use(xyz); app.use(abc),
+    however I have refactored as I believe this is a much cleaner way 
+*/
+app
+  .use(express.json)
+  .use(helmet())
+  /* 
+        This allows us to make cross origin sharing request 
+        (Needed for making API calls at another server ) 
+    */
+  .use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }))
+  .use(morgan("common"))
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: false }))
+  .use(cors());
+
+/* Routes */
+/* We will be using 4 different routes in this build */
+app
+  .use("/general", generalRoutes)
+  .use("/clients", clientRoutes)
+  .use("/management", managementRoutes)
+  .use("sales", salesRoutes);
+
+/*
+    The API's have been split as above with general looking after
+    the dashboard and user information
+*/
+
+/* Mongoose Setup */
+/* 
+    Initial port is set in the .env folder, however if this port fails
+    then it will try launch on 9000 
+*/
+const PORT = process.env.PORT || 9000;
+
+/* 
+    We use mongoose.connect and pass it the URL we set in the .env file,
+    the second parameter is an object of setup parameters
+*/
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewURLParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+  })
+  .catch((error) => {
+    console.log(`${error}`);
+  });
