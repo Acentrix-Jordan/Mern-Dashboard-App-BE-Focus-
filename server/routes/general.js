1adb624d687c1a9be436ed6c081753ffc2ef34bf
Jordan Wilson <biggyj_dev@outlook.com>
2022-12-16 18:27:35 +0000

    User Mongoose Schema Created

--- server/routes/general.js
+++ server/routes/general.js
@@ -1,5 +1,10 @@
-import express from 'express'
+import express from "express";
+import { getUser } from "../controllers/general.js";
 
 const router = express.Router();
+
+// getUser is a function from controllers
+// :id is the param we pass
+router.get("/user/:id", getUser);
 
 export default router;
